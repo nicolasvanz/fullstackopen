1900f1466f195fa94d4853b7e450da6306ae7759
nicolasvanz <nicavanz@hotmail.com>
2023-11-10 10:37:10 -0300

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 const Person = ({ person }) => <p>{person.name} {person.number}</p>
 
@@ -37,15 +38,19 @@ const PersonForm = ({
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("")
   const [filterStr, setFilterStr] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:3001/persons")
+      .then(response => {
+        const persons = response.data
+        setPersons(persons)
+      })  
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
