5dbf0de786b72002823fbe3f36ff4fb65fb529ac
nicolasvanz <nicavanz@hotmail.com>
2023-11-15 07:52:24 -0300

    exercise 3.2

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ app.get("/api/persons", (request, response) => {
     response.json(persons)
 })
 
+app.get("/info", (request, response) => {
+  response.send(
+    `
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+    `  
+  )
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
