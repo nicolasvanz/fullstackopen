db205d21114d38c98ee96b52b813ad454e79bf69
nicolasvanz <nicavanz@hotmail.com>
2023-11-16 15:38:48 -0300

    part 3b

--- part3/tests/index.js
+++ part3/tests/index.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const cors = require("cors")
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static("dist"))
 
 let notes = [
     {
@@ -72,7 +76,7 @@ app.get("/api/notes", (request, response) => {
     response.json(notes)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
