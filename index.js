61e9050ac34cf920ec644a24517bb99258240609
nicolasvanz <nicavanz@hotmail.com>
2023-11-16 16:27:22 -0300

    exercise 3.10

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 const app = express()
+
+PORT= process.env.PORT || 3001
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
 
-PORT=3001
-
 app.use(express.json())
+app.use(cors())
 
 let persons = [
     { 
