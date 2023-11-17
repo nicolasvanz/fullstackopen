3cfa8f6c28082b29acb8d12c88adb13c94f1314e
nicolasvanz <nicavanz@hotmail.com>
2023-11-17 16:32:30 -0300

    part 3c

--- part3/tests/index.js
+++ part3/tests/index.js
@@ -1,22 +1,13 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-const mongoose = require("mongoose")
 const Note = require("./models/note")
 
 const app = express()
 
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(cors())
-app.use(express.static("dist"))
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
 
 app.post("/api/notes", (request, response) => {
     const body = request.body
@@ -35,20 +26,26 @@ app.post("/api/notes", (request, response) => {
     })
 })
 
-app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+app.put("/api/notes/:id", (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+    Note.findByIdAndUpdate(request.params.id, note, {new:true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
-app.get("/api/notes/:id", (request, response) => {
-    Note.find(request.params.id).then(note => {
-        response.json(note)
-    })
-})
-
-app.get("/", (request, response) => {
-    response.send("<h1>Hello world!!<h1>")
+app.delete("/api/notes/:id", (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.get("/api/notes", (request, response) => {
@@ -56,6 +53,41 @@ app.get("/api/notes", (request, response) => {
         response.json(notes)
     })
 })
+
+app.get("/api/notes/:id", (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            error => next(error)
+        })
+})
+
+app.get("/", (request, response) => {
+    response.send("<h1>Hello world!!<h1>")
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: "unknown endpoint"})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === "CastError") {
+        return response.status(400).send({error: "malformatted id"})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
