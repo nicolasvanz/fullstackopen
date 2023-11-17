a1b3b616aceb6a5d1e02df85ecc8bd819b88e639
nicolasvanz <nicavanz@hotmail.com>
2023-11-17 15:58:13 -0300

    exercise 3.13

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
+const Person = require("./models/person")
 
 const app = express()
 
@@ -13,43 +15,18 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static("dist"))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 const generateNewRandomId = () => Math.floor(Math.random() * 10000)
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (!person) {
-    response.status(404).end()
-  } else {
+  Person.find(request.params.id).then(person =>
     response.json(person)
-  }
+  )
 })
 
 app.get("/info", (request, response) => {
---
+++ models/person.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const url = process.env.MONGODB_URI
+
+mongoose.set("strictQuery", false)
+
+console.log(`connecting to ${url}`)
+mongoose.connect(url)
+    .then(response => {
+        console.log("connected to MongoDB")
+    })
+    .catch(error => {
+        console.log(`couldn't connect to MongoDB: ${error.message}`)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: Number
+})
+
+mongoose.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model("Person", personSchema)
+
