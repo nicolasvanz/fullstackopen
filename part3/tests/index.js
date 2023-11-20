c15d0eaf834b208bfa5f5e666af6922462039c77
nicolasvanz <nicavanz@hotmail.com>
2023-11-20 13:02:38 -0300

    part 3d

--- part3/tests/index.js
+++ part3/tests/index.js
@@ -9,31 +9,28 @@ app.use(express.static("dist"))
 app.use(express.json())
 app.use(cors())
 
-app.post("/api/notes", (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-
-    if (body.content === undefined) {
-        return response.status(400).json({error: "content missing"})
-    }
-
+  
     const note = new Note({
-        content: body.content,
-        important: body.important || false
+      content: body.content,
+      important: body.important || false,
     })
-
-    note.save().then(savedNote => {
+  
+    note.save({runValidators:true})
+      .then(savedNote => {
         response.json(savedNote)
-    })
+      })
+      .catch(error => next(error))
 })
 
 app.put("/api/notes/:id", (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important
-    }
-    Note.findByIdAndUpdate(request.params.id, note, {new:true})
+    Note.findByIdAndUpdate(
+        request.params.id,
+        { content, important },
+         {new:true, runValidators:true, context: "query" })
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -83,6 +80,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "CastError") {
         return response.status(400).send({error: "malformatted id"})
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
