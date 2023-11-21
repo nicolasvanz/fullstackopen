95bfce6d22dd0ca160aa9545cbc1de8e0b45043c
nicolasvanz <nicavanz@hotmail.com>
2023-11-21 15:56:51 -0300

    exercise 4.4

--- part4/blog/tests/list_helper.test.js
+++ part4/blog/tests/list_helper.test.js
@@ -6,3 +6,21 @@ test("dummy returns one", () => {
   const result=listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "sd12k3uh1i4u12ui41g4u",
+      title:"Go to Statement considered Harmful",
+      author:"Edsger W. Dijkstra",
+      url:"http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+})
