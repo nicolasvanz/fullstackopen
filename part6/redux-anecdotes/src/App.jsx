a170fce301aa6274fff4d8619b3a9caf24b0f9ce
nicolasvanz <nicavanz@hotmail.com>
2023-12-15 13:12:44 -0300

    exercise 6.3

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,16 @@
+import Anecdotes from './components/Anecdotes'
+
+const App = () => {
+  return (
+    <div>
+      <Anecdotes/>
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ part6/redux-anecdotes/src/components/Anecdotes.jsx
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux"
+
+import { anecdoteVote } from "../reducers/anecdoteReducer"
+
+const Anecdotes = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  return (
+    <>
+    <h2>Anecdotes</h2>
+    {anecdotes.map(anecdote =>
+      <Anecdote
+        key={anecdote.id}
+        anecdote={anecdote}
+        handleVote={() => dispatch(anecdoteVote(anecdote.id))}
+      />
+    )}
+    </>
+  )
+}
+
+const Anecdote = ({ anecdote, handleVote }) => {
+  return (
+    <div key={anecdote.id}>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleVote}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+export default Anecdotes
