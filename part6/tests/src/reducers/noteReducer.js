23a0f6959f491926797aa6087baa17d1d2d3d7ab
nicolasvanz <nicavanz@hotmail.com>
2024-01-02 10:32:44 -0300

    part 6c

--- part6/tests/src/reducers/noteReducer.js
+++ part6/tests/src/reducers/noteReducer.js
@@ -1,33 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
-
 const generateId = () => {
   return (Math.random() * 100000).toFixed(0)
 }
 
 const noteReducer = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        important: false,
-        id: generateId(),
-      })
+      state.push(action.payload)
     },
     toggleImportanceOf(state, action) {
       const id = action.payload
@@ -37,11 +19,17 @@ const noteReducer = createSlice({
         important: !noteToChange.important
       }
       return state.map(note => note.id !== id ? note : changedNote)
+    },
+    appendNote(state, action) {
+      state.push(action.payload)
+    },
+    setNotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { createNote, toggleImportanceOf } = noteReducer.actions
+export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteReducer.actions
 export default noteReducer.reducer
 
 
