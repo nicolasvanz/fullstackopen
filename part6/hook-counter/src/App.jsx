e3352253e4f197d23408f7a829b7c00933745653
nicolasvanz <nicavanz@hotmail.com>
2024-01-04 09:57:44 -0300

    part 6d

---
+++ part6/hook-counter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useReducer } from 'react'
+
+const App = () => {
+  const [counter, counterDispatch] = useReducer(counterReducer, 0)
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <div>
+        <button onClick={() => counterDispatch({ type: "INC"})}>+</button>
+        <button onClick={() => counterDispatch({ type: "DEC"})}>-</button>
+        <button onClick={() => counterDispatch({ type: "ZERO"})}>0</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
