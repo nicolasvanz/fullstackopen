1525c25854c050f6a8d5fbc31627070d07ccd6a9
nicolasvanz <nicavanz@hotmail.com>
2024-01-08 10:20:21 -0300

    exercise 7.6

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -61,9 +61,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { reset: resetContent, ...content} = useField('text')
+  const { reset: resetAuthor, ...author} = useField('text')
+  const { reset: resetInfo, ...info} = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -76,9 +76,9 @@ const CreateNew = (props) => {
   }
 
   const resetAll = () => {
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
