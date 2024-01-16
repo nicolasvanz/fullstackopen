55eda0a65f028e5de1df36b5b861ecf08e1f9b53
nicolasvanz <nicavanz@hotmail.com>
2024-01-16 09:03:57 -0300

    exercise 8.4

--- part8/library/index.js
+++ part8/library/index.js
@@ -115,7 +115,7 @@ const typeDefs = `
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -124,7 +124,9 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      return books.filter(book => book.author === args.author)
+    },
     allAuthors: () => {
         return _.map(_.groupBy(books, book => book.author), (books, author) => {
           return {name: author, bookCount: books.length}
