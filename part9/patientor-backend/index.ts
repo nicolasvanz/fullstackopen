b2318d278f5e762e654e77c751f685d2fcdd6b09
nicolasvanz <nicavanz@hotmail.com>
2024-02-08 17:52:35 -0300

    exercise 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors())
 
 app.get("/api/ping", (_req, res) => {
   console.log("ping");
   res.send("pong");
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
