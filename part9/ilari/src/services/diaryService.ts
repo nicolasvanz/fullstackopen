0e9c330ba1af389ccfc8dd8711c3e9d3f9e1abf7
nicolasvanz <nicavanz@hotmail.com>
2024-02-12 11:27:23 -0300

    part 9c

--- part9/ilari/src/services/diaryService.ts
+++ part9/ilari/src/services/diaryService.ts
@@ -1,5 +1,9 @@
 import diaries from "../../data/entries";
-import { NonSensitiveDiaryEntry, DiaryEntry } from "../types";
+import {
+  NonSensitiveDiaryEntry,
+  DiaryEntry,
+  NewDiaryEntry
+} from "../types";
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
@@ -11,12 +15,23 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addDiary = () => {
-  return null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const NewDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+  diaries.push(NewDiaryEntry);
+  return NewDiaryEntry;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
 };
 
 export default {
   getEntries,
   addDiary,
-  getNonSensitiveEntries
+  getNonSensitiveEntries,
+  findById
 };
