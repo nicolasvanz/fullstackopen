5877b9a4aca98a8d36b6710dc02d729287158efd
nicolasvanz <nicavanz@hotmail.com>
2024-02-13 10:16:18 -0300

    exercise 9.15

--- part9/react/src/components/Content.tsx
+++ part9/react/src/components/Content.tsx
@@ -1,30 +1,63 @@
-interface Course {
-  name: string,
-  exerciseCount: number
+import { CoursePart } from "../types"
+import { assertNever } from "../utils"
+
+interface CoursePartsProps {
+  coursePartList: CoursePart []
 }
 
-interface CourseListProps {
-  courseList: Course []
+interface CoursePartInfoProps {
+  coursePart: CoursePart
 }
 
-interface CourseInfoProps {
-  course: Course
-}
-
-const CourseInfo = (props: CourseInfoProps) => {
+const CoursePartInfo = (props: CoursePartInfoProps) => {
+  const part = props.coursePart
+  switch(part.kind) {
+    case "basic":
+      return (
+        <div>
+          <h3>{part.name} {part.exerciseCount}</h3>
+          <i>{part.description}</i>
+        </div>
+      )
+    case "group":
+      return (
+        <div>
+          <h3>{part.name} {part.exerciseCount}</h3>
+          <i>project exercises {part.groupProjectCount}</i>
+        </div>
+      )
+    case "background":
+      return (
+        <div>
+          <h3>{part.name} {part.exerciseCount}</h3>
+          <i>{part.description}</i>
+          <p>submit to {part.backgroundMaterial}</p>
+        </div>
+      )
+    case "special":
+      return (
+        <div>
+          <h3>{part.name} {part.exerciseCount}</h3>
+          <i>{part.description}</i>
+          <p>required skills: {part.requirements.join(', ')}</p>
+        </div>
+      )
+    default:
+      assertNever(part)
+  }
   return (
     <p>
-      {props.course.name} {props.course.exerciseCount}
+      {props.coursePart.name} {props.coursePart.exerciseCount}
     </p>
   )
 }
 
-const CourseList = (props: CourseListProps) => {
+const CourseParts = (props: CoursePartsProps) => {
   return (
-      props.courseList.map(
-        course => <CourseInfo key={course.name} course={course}/>
-      )
+    props.coursePartList.map(
+      part => <CoursePartInfo key={part.name} coursePart={part}/>
+    )
   )
 }
 
-export default CourseList
+export default CourseParts
