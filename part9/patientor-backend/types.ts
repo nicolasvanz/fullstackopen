665ae874b41794b468e9bd036567eea8f09e079d
nicolasvanz <nicavanz@hotmail.com>
2024-02-15 17:27:37 -0300

    exercise 9.22

--- part9/patientor-backend/types.ts
+++ part9/patientor-backend/types.ts
@@ -10,8 +10,48 @@ export enum Gender {
   Other = "other"
 }
 
-export interface Entry {
+export interface BaseEntry {
+  id: string,
+  description: string,
+  date: string,
+  specialist: string,
+  diagnosisCodes?: Array<Diagnosis["code"]>
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck",
+  healthCheckRating: HealthCheckRating
+}
+
+export interface Discharge {
+  date: string,
+  criteria: string,
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital",
+  discharge: Discharge
+}
+
+export interface SickLeave {
+  startDate: string,
+  endDate: string,
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare"
+  employerName: string,
+  sickLeave?: SickLeave
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -26,3 +66,7 @@ export interface Patient {
 export type NewPatientEntry = Omit<Patient, 'id'>;
 
 export type NonSensetivePatient = Omit<Patient, 'ssn' | 'entries'>;
+
+export type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
