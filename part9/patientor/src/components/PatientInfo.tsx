103fff02d5e1f7782f225c96cfed37ba2f1d0a4f
nicolasvanz <nicavanz@hotmail.com>
2024-02-15 19:12:16 -0300

    exercise 9.25

--- part9/patientor/src/components/PatientInfo.tsx
+++ part9/patientor/src/components/PatientInfo.tsx
@@ -1,21 +1,144 @@
 import { useEffect, useState } from "react";
+
 import FemaleIcon from '@mui/icons-material/Female';
 import MaleIcon from '@mui/icons-material/Male';
+import WorkIcon from '@mui/icons-material/Work';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
 
-import {Diagnosis, Entry, Patient} from "../types";
+import {Diagnosis, Entry, HealthCheckEntry, HealthCheckRating, HospitalEntry, OccupationalHealthcareEntry, Patient} from "../types";
 import patientsService from "../services/patients";
 import diagnosesService from "../services/diagnoses";
+import { assertNever } from "../utils";
 
 interface PatientInfoProps {
   patientId: string
 }
 
-interface PatientEntryProps {
-  entry: Entry
+interface PatientEntryInfoProps {
+  entry: Entry,
+  diagnoses: Array<Diagnosis>
 }
 
-const PatientEntry = (props: PatientEntryProps) => {
+interface PatientInfoPropsBase {
+  entry: Entry,
+  findDiagnoseByCode: (code: Diagnosis["code"]) => Diagnosis | undefined
+}
+
+interface HealthCheckEntryInfoProps extends PatientInfoPropsBase {
+  entry: HealthCheckEntry,
+}
+
+interface HospitalEntryInfoProps extends PatientInfoPropsBase {
+  entry: HospitalEntry
+}
+
+interface OccupationalHealthcareEntryInfoProps extends PatientInfoPropsBase {
+  entry: OccupationalHealthcareEntry
+}
+
+const PatientEntry = (props: PatientEntryInfoProps) => {
   const entry = props.entry;
+  
+  const findDiagnoseByCode = (code: Diagnosis["code"]): Diagnosis | undefined => {
+    return props.diagnoses.find(d => d.code === code);
+  };
+
+  const style = {
+    borderStyle: "solid",
+    borderRadius: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    paddingLeft: 10
+  };
+
+  switch(entry.type) {
+    case "HealthCheck":
+      return <div style={style}>
+        <HealthCheckEntryInfo entry={entry as HealthCheckEntry} findDiagnoseByCode={findDiagnoseByCode} />
+      </div>;
+    case "Hospital":
+      return <div style={style}>
+        <HospitalEntryInfo entry={entry as HospitalEntry} findDiagnoseByCode={findDiagnoseByCode}/>
+      </div>;
+    case "OccupationalHealthcare":
+      return <div style={style}>
+        <OccupationalHealthcareEntryInfo entry={entry as OccupationalHealthcareEntry} findDiagnoseByCode={findDiagnoseByCode}/>
+      </div>;
+    default:
+      assertNever(entry);
+  }
+};
+
+const HealthCheckEntryInfo = (props: HealthCheckEntryInfoProps) => {
+  const entry = props.entry;
+  return (
+    <div>
+      <p>{entry.date} <MedicalServicesIcon /></p>
+      {entry.description}
+      <ul>
+        {
+          entry.diagnosisCodes &&
+          entry.diagnosisCodes.map(dc => {
+            const diagnosisObject = props.findDiagnoseByCode(dc);
+            return <li key={dc}>{dc} {diagnosisObject ? diagnosisObject.name : null}</li>;
+          })
+        }
+      </ul>
+      {entry.healthCheckRating === HealthCheckRating.Healthy && <FavoriteIcon style={{color: "green"}} />}
+      {entry.healthCheckRating === HealthCheckRating.LowRisk && <FavoriteIcon style={{color: "yellow"}} />}
+      {entry.healthCheckRating === HealthCheckRating.HighRisk && <FavoriteIcon style={{color: "orange"}} />}
+      {entry.healthCheckRating === HealthCheckRating.CriticalRisk && <FavoriteIcon style={{color: "red"}} />}
+      <br />
+      diagnose by {entry.specialist}
+    </div>
+  );
+};
+
+const HospitalEntryInfo = (props: HospitalEntryInfoProps) => {
+  const entry = props.entry;
+  return (
+    <div>
+      <p>{entry.date} <LocalHospitalIcon /></p>
+      {entry.description}
+      <ul>
+        {
+          entry.diagnosisCodes &&
+          entry.diagnosisCodes.map(dc => {
+            const diagnosisObject = props.findDiagnoseByCode(dc);
+            return <li key={dc}>{dc} {diagnosisObject ? diagnosisObject.name : null}</li>;
+          })
+        }
+      </ul>
+      {entry.discharge.criteria} at {entry.discharge.date}
+      <p>diagnose by {entry.specialist}</p>
+    </div>
+  );
+};
+
+const OccupationalHealthcareEntryInfo = (props: OccupationalHealthcareEntryInfoProps) => {
+  const entry = props.entry;
+  return (
+    <div>
+      <p>{entry.date}<WorkIcon /> {entry.employerName}</p>
+      {entry.description}
+      <ul>
+        {
+          entry.diagnosisCodes &&
+          entry.diagnosisCodes.map(dc => {
+            const diagnosisObject = props.findDiagnoseByCode(dc);
+            return <li key={dc}>{dc} {diagnosisObject ? diagnosisObject.name : null}</li>;
+          })
+        }
+      </ul>
+      diagnose by {entry.specialist}
+    </div>
+  );
+};
+
+const PatientInfo = (props: PatientInfoProps) => {
+  const [patient, setPatient] = useState<Patient>();
   const [diagnoses, setDiagnoses] = useState<Array<Diagnosis>>([]);
 
   useEffect(() => {
@@ -26,29 +149,6 @@ const PatientEntry = (props: PatientEntryProps) => {
     };
     fetchDiagnoses();
   });
-
-  const findDiagnoseByCode = (code: Diagnosis["code"]): Diagnosis | undefined => {
-    return diagnoses.find(d => d.code === code);
-  };
-
-  return (
-    <div>
-      <p>{entry.date} {entry.description}</p>
-      <ul>
-        {
-          entry.diagnosisCodes &&
-          entry.diagnosisCodes.map(dc => {
-            const diagnosisObject = findDiagnoseByCode(dc);
-            return <li key={dc}>{dc} {diagnosisObject ? diagnosisObject.name : null}</li>;
-          })
-        }
-      </ul>
-    </div>
-  );
-};
-
-const PatientInfo = (props: PatientInfoProps) => {
-  const [patient, setPatient] = useState<Patient>();
 
   useEffect(() => {
     const fecthPatient = async () => {
@@ -71,7 +171,7 @@ const PatientInfo = (props: PatientInfoProps) => {
         <h3>Entries</h3>
         {
           patient.entries.map(entry =>
-            <PatientEntry key={entry.id} entry={entry}/>
+            <PatientEntry key={entry.id} entry={entry} diagnoses={diagnoses}/>
           )
         }
       </div>
