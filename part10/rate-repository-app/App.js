1463c2b6f9b2e3e4c36b733055e7d1999c6a7679
nicolasvanz <nicavanz@hotmail.com>
2024-02-21 17:37:49 -0300

    exercise 10.3

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import RepositoryList from "./src/components/RepositoryList"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  return <RepositoryList />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
